d13fe84709393146c0ae379e8c5ea98f9c433379
imagebutton2 <1046509851@qq.com>
2018-12-13 15:12:35 +0800

    创建一个Test类，观察在main方法中的初始值

---
+++ Test/Test.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class Test {
+
+	    public int i;
+	    public char a;
+	    public void fun(){
+	        System.out.println("i="+i+"\n"+"a="+a);
+	    }
+	   
+	    public static void main(String[] args)
+	    {
+	        new Test().fun();
+	    }
+}
+
