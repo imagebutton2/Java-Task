4986053a23a2be7fb246a05d72612e213f017aee
imagebutton2 <1046509851@qq.com>
2019-02-16 22:19:03 +0800

    MenuTest

---
+++ menu/MenuTest.java
@@ -0,0 +1,16 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MenuTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         JFrame frame = new MenuFrame();
+         frame.setTitle("MenuTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
