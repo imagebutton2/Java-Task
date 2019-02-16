cdfb18178fc02f96d3fa719f6bd06fc779edf54e
imagebutton2 <1046509851@qq.com>
2019-02-16 22:20:08 +0800

    BorderTest

---
+++ border/BorderTest.java
@@ -0,0 +1,15 @@
+
+import java.awt.*;
+import javax.swing.*;
+public class BorderTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         JFrame frame = new BorderFrame();
+         frame.setTitle("BorderTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
