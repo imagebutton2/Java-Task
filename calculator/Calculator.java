b1f605cbe082ae5b622c356a9ea9b43c096d6c65
imagebutton2 <1046509851@qq.com>
2019-02-17 21:26:50 +0800

    Calculator

---
+++ calculator/Calculator.java
@@ -0,0 +1,15 @@
+
+import java.awt.*;
+import javax.swing.*;
+public class Calculator
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         CalculatorFrame frame = new CalculatorFrame();
+         frame.setTitle("Calculator");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
---
+++ calculator/CalculatorFrame.java
@@ -0,0 +1,10 @@
+
+import javax.swing.*;
+public class CalculatorFrame extends JFrame
+{
+   public CalculatorFrame()
+   {
+      add(new CalculatorPanel());
+      pack();
+   }
+}
