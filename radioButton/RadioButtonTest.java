6509d96bad801603ed09c67aca6caf9d42ae18ac
imagebutton2 <1046509851@qq.com>
2019-02-19 20:55:47 +0800

    RadioButtonTest

---
+++ radioButton/RadioButtonTest.java
@@ -0,0 +1,15 @@
+
+import java.awt.*;
+import javax.swing.*;
+public class RadioButtonTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         JFrame frame = new RadioButtonFrame();
+         frame.setTitle("RadioButtonTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
